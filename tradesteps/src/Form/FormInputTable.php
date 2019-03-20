7558f9a7d811d5fbeb60fc432aaf12189bf01dd8
limelime <limelime@users.noreply.github.com>
2019-03-20 21:18:26 +0000

    Add example.

---
+++ tradesteps/src/Form/FormInputTable.php
@@ -0,0 +1,55 @@
+<?php
+namespace Drupal\tradesteps\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+class FormInputTable extends FormBase {
+
+    public function getFormId() {
+        return 'tradesteps_form_input_table';
+    }
+
+    public function buildForm(array $form, FormStateInterface $form_state) {
+        
+        // Add the headers.
+        $form['contacts'] = array(
+                                    '#type' => 'table',
+                                    '#title' => 'Sample Table',
+                                    '#header' => array('Name', 'Phone'),
+                                );
+        
+        // Add input fields in table cells.
+        for ($i=1; $i<=4; $i++) {
+            $form['contacts'][$i]['name'] = array(
+                                                '#type' => 'textfield',
+                                                '#title' => t('Name'),
+                                                '#title_display' => 'invisible',
+                                                '#default_value' => 'name'.$i,
+                                            );
+
+            $form['contacts'][$i]['phone'] = array(
+                                                '#type' => 'tel',
+                                                '#title' => t('Phone'),
+                                                '#title_display' => 'invisible',
+                                                '#default_value' => '763-999-444'.$i,
+                                            );
+        }
+    
+    
+        // Add a submit button that handles the submission of the form.
+        $form['actions']['submit'] = [
+                                        '#type' => 'submit',
+                                        '#value' => $this->t('Submit'),
+                                    ];
+
+        return $form;
+    }
+
+    public function submitForm(array &$form, FormStateInterface $form_state) {
+        // Find out what was submitted.
+        $values = $form_state->getValues();
+        drupal_set_message(print_r($values['contacts'],true));
+    }
+
+}
